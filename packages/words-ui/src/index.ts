ad1f9c4866d8199cb020eea2843ec68fd603c78b
Алексей Иванов <alexey.ivanov@travelline.ru>
2023-03-18 16:29:27 +0300

    Improve some code

--- packages/words-ui/src/index.ts
+++ packages/words-ui/src/index.ts
@@ -1,10 +1,6 @@
 export * from "../../words/src/context";
 export * from "./components/index";
 export * from "./hooks";
+export * from "./services";
 export * from "./theme/index";
 export * from "./types";
-
-
-
-
-
