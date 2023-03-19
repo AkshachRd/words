eea90a08fe2035b3671665d749a14f17669a7c48
Daniil <dr.chashka@gmail.com>
2023-03-19 17:55:51 +0300

    ShuffleArray function refactored. ESLint errors resolved.

--- packages/words-ui/src/services/ShuffleArray.ts
+++ packages/words-ui/src/services/ShuffleArray.ts
@@ -1,14 +1,1 @@
-export const shuffleArray = <T>(array: T[]): T[] => {
-  let currentIndex = array.length;
-  // While there remain elements to shuffle.
-  while (currentIndex != 0) {
-    // Pick a remaining element.
-    const randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
-  }
-
-  return array;
-};
+export const shuffleArray = <T>([...array]: T[]): T[] => array.sort((a, b) => 0.5 - Math.random());
