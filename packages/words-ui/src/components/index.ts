dcd03b57e18fb8691a2f70c2ba870ac2307adddf
Алексей Иванов <alexey.ivanov@travelline.ru>
2023-04-03 18:29:52 +0300

    Fix

--- packages/words-ui/src/components/index.ts
+++ packages/words-ui/src/components/index.ts
@@ -1,7 +1,7 @@
+export * from "./AddCard";
 export * from "./Card";
 export * from "./CardEditor";
 export * from "./CardList";
 export * from "./CardTrainer";
 export * from "./FlipCard";
 export * from "./Header";
-export { default } from "./AddCard";
